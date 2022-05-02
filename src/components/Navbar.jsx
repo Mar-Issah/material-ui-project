5802561c97b201bd52d88077036c1c6859565b59
Mar-Issah <masy370@gmail.com>
2022-05-01 22:19:27 -0400

    Complete sidebar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,11 @@
-import { AppBar, Typography, InputBase, Badge, Avatar } from '@mui/material';
+import { AppBar, Typography, InputBase, Badge, Avatar, Menu, MenuItem } from '@mui/material';
 import LocalFloristIcon from '@mui/icons-material/LocalFlorist';
 import { Mail, Notifications } from '@mui/icons-material';
 import { StyledToolbar, IconsContainer, Search, UserContainer } from '../styled/Navbar';
+import { useState } from 'react';
 
 const Navbar = () => {
-  //my own styles
-
+  const [open, setOpen] = useState(false);
   return (
     <AppBar position='sticky'>
       <StyledToolbar>
@@ -24,13 +24,31 @@ const Navbar = () => {
           <Badge badgeContent={4} color='error'>
             <Notifications />
           </Badge>
-          <Avatar alt='myavatar' src='' sx={{ width: '30px', height: '30px' }} />
+          <Avatar alt='myavatar' src='' sx={{ width: '30px', height: '30px' }} onClick={() => setOpen(true)} />
         </IconsContainer>
-        <UserContainer>
+        <UserContainer onClick={() => setOpen(true)}>
           <Avatar alt='myavatar' src='' sx={{ width: '30px', height: '30px' }} />
           <Typography variant='span'>Marsiya</Typography>
         </UserContainer>
       </StyledToolbar>
+      <Menu
+        id='demo-positioned-menu'
+        aria-labelledby='demo-positioned-button'
+        open={open}
+        onClose={() => setOpen(false)}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
     </AppBar>
   );
 };
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,81 @@
-import { Box } from '@mui/material';
+import { Box, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Switch } from '@mui/material';
+import { Home, Article, Groups, Person, Settings, AccountBox, Storefront, DarkMode } from '@mui/icons-material';
 
 const Sidebar = () => {
   return (
-    <Box bgcolor='pink' flex={1} p={2} sx={{ display: { xs: 'none', sm: 'block' } }}>
-      Sidebar
+    <Box flex={1} sx={{ display: { xs: 'none', sm: 'block' } }}>
+      <nav>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+                <Home />
+              </ListItemIcon>
+              <ListItemText primary='Homepage' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+                <Article />
+              </ListItemIcon>
+              <ListItemText primary='Pages' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+                <Groups />
+              </ListItemIcon>
+              <ListItemText primary='Groups' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#home'>
+              <ListItemIcon>
+                <Storefront />
+              </ListItemIcon>
+              <ListItemText primary='Marketplace' />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#sample'>
+              <ListItemIcon>
+                <Person />
+              </ListItemIcon>
+              <ListItemText primary='Friends' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#sample'>
+              <ListItemIcon>
+                <Settings />
+              </ListItemIcon>
+              <ListItemText primary='Settings' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton component='a' href='#sample'>
+              <ListItemIcon>
+                <AccountBox />
+              </ListItemIcon>
+              <ListItemText primary='Profile' />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <DarkMode />
+              </ListItemIcon>
+              <Switch />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </nav>
     </Box>
   );
 };
