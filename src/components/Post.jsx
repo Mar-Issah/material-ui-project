fa2ffcbeb7bb26774dccebb2ba07b4cff7402171
Mar-Issah <masy370@gmail.com>
2022-05-16 13:47:49 -0400

    Add avatar in post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,34 +1,54 @@
-import { Typography, Card, CardHeader, CardMedia, CardContent, CardActions, Avatar, IconButton, Checkbox } from '@mui/material';
-import { MoreVert, Favorite, FavoriteBorder, Share } from '@mui/icons-material';
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CardActions,
+  Avatar,
+  IconButton,
+  Checkbox,
+} from "@mui/material";
+import { MoreVert, Favorite, FavoriteBorder, Share } from "@mui/icons-material";
 const Post = () => {
   return (
-    <Card sx={{ margin: '5px' }}>
+    <Card sx={{ margin: "5px" }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: 'red' }} aria-label='recipe'>
-            R
+          <Avatar sx={{ bgcolor: "red" }} aria-label="recipe">
+            Pic
           </Avatar>
         }
         action={
-          <IconButton aria-label='settings'>
+          <IconButton aria-label="settings">
             <MoreVert />
           </IconButton>
         }
-        title='Siya Siya Barbados'
-        subheader='September 14, 2016'
+        title="Siya Siya Barbados"
+        subheader="September 14, 2016"
       />
-      <CardMedia component='img' height='20%' image='https://res.cloudinary.com/dytnpjxrd/image/upload/v1650326872/MERN%20Ecommerce/pexels-mart-production-7605201_sa4c7q.jpg' alt='Lady on computer' />
+      <CardMedia
+        component="img"
+        height="20%"
+        image="https://res.cloudinary.com/dytnpjxrd/image/upload/v1650326872/MERN%20Ecommerce/pexels-mart-production-7605201_sa4c7q.jpg"
+        alt="Lady on computer"
+      />
       <CardContent>
-        <Typography variant='body2' color='text.secondary'>
-          This impressive paella is a perfect party dish and a fun meal to cook together with your guests. Add 1 cup of frozen peas along with the mussels, if you like.
+        <Typography variant="body2" color="text.secondary">
+          This impressive paella is a perfect party dish and a fun meal to cook
+          together with your guests. Add 1 cup of frozen peas along with the
+          mussels, if you like.
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label='add to favorites'>
+        <IconButton aria-label="add to favorites">
           {/* <Favorite /> */}
-          <Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite sx={{ color: 'red' }} />} />
+          <Checkbox
+            icon={<FavoriteBorder />}
+            checkedIcon={<Favorite sx={{ color: "red" }} />}
+          />
         </IconButton>
-        <IconButton aria-label='share'>
+        <IconButton aria-label="share">
           <Share />
         </IconButton>
       </CardActions>
