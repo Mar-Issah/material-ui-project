79eca0e7115ebfdc31aae166c15498b41c9b0a38
Mar-Issah <masy370@gmail.com>
2022-05-16 13:48:56 -0400

    Add more post component in Feed page

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,8 @@
-import { Box } from '@mui/material';
-import Post from './Post';
+import { Box } from "@mui/material";
+import Post from "./Post";
 
 const Feed = () => {
-  //we can pass in title, image, text etc in the Post as props
+  //we can pass in title, image, text etc in the post as props
   return (
     <Box flex={4} p={2}>
       <Post />
